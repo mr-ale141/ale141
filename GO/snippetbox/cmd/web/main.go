2800016a3ac926fa198916a1431ee45d73eb159e
mr-ale141 <ale141@rambler.ru>
2023-03-26 14:19:34 +0300

    Snippetbox add first static files

--- GO/snippetbox/cmd/web/main.go
+++ GO/snippetbox/cmd/web/main.go
@@ -11,6 +11,9 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Запуск сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
