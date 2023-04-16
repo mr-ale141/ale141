decb06b5da1308f4d87a040e5750367fcab74ca5
mr-ale141 <ale141@rambler.ru>
2023-04-16 12:58:49 +0300

    ForDS120j v.1.0

--- PHP/ForDS120j/index.php
+++ PHP/ForDS120j/index.php
@@ -1,17 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
-  <title>Document</title>
+  <title>Афоризмы</title>
   <link rel="stylesheet" type="text/css" href="./index.css">
+  <link rel="shortcut icon" href="./lamp.ico" type="image/x-icon">
 </head>
+
 <body>
   <h1>Случайный афоризм</h1>
+  <a href="/index.php">Следующий</a>
   <p>
     <?PHP
-      print "Hello!";
+    $content = file_get_contents('./aforizm.txt');
+    $aforizms = explode("\n", $content);
+    $ramd_num = rand(0, count($aforizms) - 1);
+    print($aforizms[$ramd_num]);
     ?>
   </p>
-  <button>Следующий</button>
 </body>
+
 </html>
