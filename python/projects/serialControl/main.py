406e55cdf2b7dc77da8cbe3087b8478d4ebec9ee
mr-ale141 <ale141@rambler.ru>
2023-12-04 00:31:46 +0300

    python serial controle added print data

--- python/projects/serialControl/main.py
+++ python/projects/serialControl/main.py
@@ -30,6 +30,7 @@ def onRead():
         ui.graph.clear()
         ui.graph.plot([-60, -50, -40, -30, -20, -10],
                       [int(data[1]), int(data[2]), int(data[3]), int(data[4]), int(data[5]), int(data[6])])
+    print(data)
 
 def onOpen():
     serial.setPortName(ui.comList.currentText())
